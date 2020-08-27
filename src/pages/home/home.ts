dbb402ff99c7efc989fd1e28f5fef76e391789ed
eduardo-af <laguirre@quaxar.com>
2020-08-27 17:07:47 -0500

    interfaz lista

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
@@ -6,8 +6,18 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @Input() ngStyle: { [klass: string]: any; }
 
   accion: Object[];
+
+  numeros = [
+    [7,8,9],
+    [4,5,6],
+    [1,2,3],
+    [0]
+  ]
+
+  cartas = [1,2,3];
 
   constructor(public navCtrl: NavController, navParams: NavParams) {
     
