8baac0940e2d4a8ad869c4541188c54176602bdd
eduardo-af <laguirre@quaxar.com>
2020-08-28 15:24:56 -0500

    logica agregada

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -36,22 +36,26 @@ export class HomePage {
     this.inputFocus = e-1;
   }
 
-  botonPresionado(n){
+  botonPresionado(valor){
     if( this.inputFocus == null || this.inputFocus == 2 ){
       console.log("Selecciona primero una entrada válida");
 
     }else{
       if( this.entradas[this.inputFocus].digito == "0"){
-        this.entradas[this.inputFocus].digito = n;
+        this.entradas[this.inputFocus].digito = valor;
 
-      }else if( n == 0 && this.entradas[this.inputFocus].digito == "0" ){
-        this.entradas[this.inputFocus].digito = n;
+      }else if( valor == 0 && this.entradas[this.inputFocus].digito == "0" ){
+        this.entradas[this.inputFocus].digito = valor;
 
       }else{
-        this.entradas[this.inputFocus].digito += n.toString()
+        this.entradas[this.inputFocus].digito += valor.toString()
       }
 
     }
+  }
+
+  evaluar(){ //evalua una cadena de string y realiza la operacion aritmetica
+    this.entradas[2].digito = eval( this.entradas[0].digito + this.accion['valor'] + this.entradas[1].digito );
   }
 
 
